1309ef27e33a9f0751d97e84d340fb5aa69018c5
juan camilo <santiago14.parraga@gmail.com>
2023-10-10 14:03:59 -0500

    prueba

--- src/main/java/co/edu/sena/SystemCarpet/model/accesos/RolPermiso.java
+++ src/main/java/co/edu/sena/SystemCarpet/model/accesos/RolPermiso.java
@@ -1,10 +1,10 @@
 package co.edu.sena.SystemCarpet.model.accesos;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
 
 @Setter
 @Getter
@@ -13,5 +13,12 @@ import lombok.Setter;
 @Table(name = "roles_permisos")
 public class RolPermiso {
 
+    @ManyToOne
+    @JoinColumn(name = "rol_id",nullable = false)
+    private Rol rol;
+
+    @ManyToOne
+    @JoinColumn(name = "permiso_id",nullable = false)
+    private Permiso permiso;
 }
 
